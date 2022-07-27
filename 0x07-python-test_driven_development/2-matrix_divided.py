47773344d989ca395f822d018d8382d433b89515
asche16 <aschalewwolde850@gmail.com>
2022-07-27 07:18:34 -0700

    fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
+"""
+    Contain function that handles matrix division
+"""
+
+
 def matrix_divided(matrix, div):
-    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
-    if not matrix:
-        raise TypeError(errorMessage)
+    """Divides all elements of a matrix"""
+
     if not isinstance(matrix, list):
-        raise TypeError(errorMessage)
-    for lists in matrix:
-        if not isinstance(lists, list):
-            raise TypeError(errorMessage)
-        for item in lists:
-            if not isinstance(item, int) and not isinstance(item, float):
-                raise TypeError(errorMessage)
-    for lists in matrix:
-        if len(lists) == 0:
-            raise TypeError(errorMessage)
-    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(v, list) for v in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(v, (int, float)) for row in matrix for v in row):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+
+    for index, row in enumerate(matrix):
+        if len(row) != len(matrix[index - 1]):
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    if not all(len(lists) == len(matrix[0]) for lists in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(item / div, 2) for item in lists] for lists in matrix]
+
+    return list(map(lambda row: list(map(lambda v:
+                                         round(v/div, 2), row)), matrix))
