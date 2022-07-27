41abc8293253f129982b89208a2169c64467769e
asche16 <aschalewwolde850@gmail.com>
2022-07-27 07:30:20 -0700

    fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""text indent"""
+"""
+    Contains a single function that handles text indenting
+"""
 
 
 def text_indentation(text):
-    """text indent"""
+    """
+        prints a text with 2 new lines after each
+        of these characters: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for delimeter in "?:.":
-        words = (delimeter + "\n\n").join(
-                [index.strip(" ") for index in words.split(delimeter)])
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    begin = 0
+    delimeters = [".", "?", ":"]
+    for c in text:
+        if c.isspace() and begin == 0:
+            continue
+        elif c in delimeters:
+            print("{}\n\n".format(c), end="")
+            begin = 0
+            continue
+        else:
+            print("{}".format(c), end="")
+            begin += 1
