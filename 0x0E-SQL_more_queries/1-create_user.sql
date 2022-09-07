42c3577dadb98eae8025a42a33695f95b61a4fe7
asche16 <aschalewwolde850@gmail.com>
2022-09-07 05:27:24 -0700

    all_update

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- create new user user_0d_1, full privilege and has password
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- creates a table with a default and unique id
+CREATE TABLE IF NOT EXISTS unique_id(id INT unique default 1, name VARCHAR(256));
