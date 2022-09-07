631fcee339e2259315644e012093bd8a6ccfac56
asche16 <aschalewwolde850@gmail.com>
2022-09-07 05:49:46 -0700

    fix

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- list all privileges of 2 MYSQL users
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
